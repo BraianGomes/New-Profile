dc5a9cb3f184a6a868c7d2cc2fd752279d56f476
Braian de Carvalho Gomes <gomes.braian88@gmail.com>
2023-09-05 10:22:47 +1000

    updating

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let allSection = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  allSection.forEach((sec) => {
+    let backTop = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (backTop >= offset && backTop < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*= " + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  /*////////////////////////////////////////////////////*/
+
+  let menuBar = document.querySelector("#logo");
+  let navbar = document.querySelector("#navbar");
+
+  menuBar.onclick = () => {
+    menuBar.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+  };
+
+  navbar.classList.remove('active');
+};
